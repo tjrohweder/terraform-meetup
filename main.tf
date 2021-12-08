36c962a24116006970024d6a0c4df468485c9ace
Taina Rohweder <taina.rohweder@ilegra.com>
2021-12-08 00:22:51 -0300

    Changing required terraform version

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = ">= 3"
+  }
+
+  backend "s3" {
+    bucket  = "tjrohweder-production-terraform"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
 }
